ad79d691147d7955d18709ab9abb30c56d204bed
vichanse <vicky.nsenga@gmail.com>
2019-06-14 06:01:03 +0200

    Interacting with input boxes

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -94,4 +94,23 @@ describe('HeroesComponent (deep tests)', () => {
 
 
     })
+
+    it('should add a new hero to the hero list when the add button is clicked', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+        fixture.detectChanges();
+        const name = "Mr. Nice";
+        mockHeroService.addHero.and.returnValue(of({id: 5, name, strength: 4}));
+        const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+        const addButton = fixture.debugElement.queryAll(By.css('button'))[0];
+
+        inputElement.value = name;
+        addButton.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
+
+        expect(heroText).toContain(name);
+
+    })
 });
