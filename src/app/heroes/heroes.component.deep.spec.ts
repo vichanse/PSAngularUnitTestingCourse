a4309b61960362b07eb7963587def6fa84706f06
vichanse <vicky.nsenga@gmail.com>
2019-06-14 05:38:39 +0200

    Triggering Events on Elements

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -49,4 +49,19 @@ describe('HeroesComponent (deep tests)', () => {
         }
         
     })
+
+    it(`should call heroService.deleteHero when the Hero Component's delete is clicked`, () => {
+        spyOn(fixture.componentInstance, 'delete');
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+        fixture.detectChanges();
+
+        const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+        heroComponents[0].query(By.css('button')).triggerEventHandler('click', {stopPropagation: () => {}});
+
+        expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+
+
+    })
 });
